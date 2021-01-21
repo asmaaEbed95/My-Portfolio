de61ce4dbc413f1c42032b2aa94c0ded49610575
asmaa_ebed <asmaaebed95@gmail.com>
2021-01-21 14:54:37 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -289,31 +289,6 @@
                 email: "Please enter valid Email", //Write here your error message that you want to show in contact form
                 subject: "Please enter your Subject", //Write here your error message that you want to show in contact form
                 message: "Please write your Message" //Write here your error message that you want to show in contact form
-            },
-
-            submitHandler: function(form) {
-                $('#send').attr({ 'disabled': 'true', 'value': 'Sending...' });
-                $.ajax({
-                    type: "POST",
-                    url: "email.php",
-                    data: $(form).serialize(),
-                    success: function() {
-                        $('#send').removeAttr('disabled').attr('value', 'Send');
-                        $("#success").slideDown("slow");
-                        setTimeout(function() {
-                            $("#success").slideUp("slow");
-                        }, 5000);
-                        form.reset();
-                    },
-                    error: function() {
-                        $('#send').removeAttr('disabled').attr('value', 'Send');
-                        $("#error").slideDown("slow");
-                        setTimeout(function() {
-                            $("#error").slideUp("slow");
-                        }, 5000);
-                    }
-                });
-                return false; // required to block normal submit since you used ajax
             }
 
         });
